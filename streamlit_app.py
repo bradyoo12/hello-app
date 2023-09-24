8dea3ddca70eb17d11a5f9680da84ffab0c0740f
Brad Yoo <byoo@janison.com>
2023-09-25 00:25:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,19 @@
 import streamlit as st
 import vertexai
 from vertexai.language_models import TextGenerationModel
+# st.write('Hello world!')
 
-st.write('Hello world!')
+vertexai.init(project="mycloudml-337117", location="us-central1")
+parameters = {
+    "candidate_count": 1,
+    "max_output_tokens": 256,
+    "temperature": 0.2,
+    "top_p": 0.8,
+    "top_k": 40
+}
+model = TextGenerationModel.from_pretrained("text-bison@001")
+response = model.predict(
+    """is streamlit a framework?""",
+    **parameters
+)
+st.write(f"{response.text}")
